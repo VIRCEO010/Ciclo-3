27668666bc1a7f3ec52a2aaf0e4f669cf2b7e7c5
VIRCEO010 <111552863+VIRCEO010@users.noreply.github.com>
2022-09-30 12:11:54 -0500

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,148 @@
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from django import forms
+from .models import *
+
+#FORMS DE VETERINARIO
+class VeterinarioForm(forms.ModelForm):
+    username = forms.CharField(max_length=20, label='Usuario',widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': "Nombre de usuario"
+        }
+    ))
+    password = forms.CharField(max_length=120, label='contraseña', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    rol=forms.ModelChoiceField(label='Rol', queryset=Group.objects.all(), widget=forms.Select(
+        attrs={
+            'class':'form-select'
+        }
+    ))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={
+            'class':'form-control'
+        }
+    ))
+    nombres= forms.CharField(max_length=120, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    apellidos= forms.CharField(max_length=120, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    tipo_documento= forms.CharField(max_length=20, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    num_documento= forms.CharField(max_length=20, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    celular= forms.CharField(max_length=10, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    num_profesional= forms.CharField(max_length=10, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    class Meta:
+        model= Veterinario
+        fields= ['num_profesional']
+
+#FORMS DE CLIENTE
+class ClienteForm(forms.ModelForm):
+    username = forms.CharField(max_length=20, label='Usuario',widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': "Nombre de usuario"
+        }
+    ))
+    password = forms.CharField(max_length=120, widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ))
+    rol=forms.ModelChoiceField(label='Rol', queryset=Group.objects.all(), widget=forms.Select(
+        attrs={
+            'class':'form-select'
+        }
+    ))
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={
+            'class':'form-control'
+        }
+    ))
+    nombres= forms.CharField(max_length=120, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    apellidos= forms.CharField(max_length=120, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    tipo_documento= forms.CharField(max_length=20, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    num_documento= forms.CharField(max_length=20, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    celular= forms.CharField(max_length=10, widget=(forms.TextInput(
+        attrs={
+            'class': "form-control"
+        }
+    )))
+    class Meta:
+        model= cliente
+        fields= []
+
+#FORMS DE GRUPOS
+class GroupsForm(forms.ModelForm):
+    name = forms.CharField(max_length=80, label='Rol', widget=forms.TextInput(
+        attrs={'class': 'form-control'
+        }
+    ))
+    permisos=forms.ModelMultipleChoiceField(label='Permisos',
+        queryset=Permission.objects.filter(content_type__app_label='veterinariaapp'), 
+        widget=forms.SelectMultiple(
+            attrs={
+                'class': 'form-select'
+            }
+        )
+    )
+    class Meta:
+        model= Group
+        fields= '__all__'
+
+#FORMS PARA EL LOGIN
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=80, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Usuario'
+        }
+    ))
+    
+    password = forms.CharField(max_length=80, widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'password'
+        }
+    ))
+    
